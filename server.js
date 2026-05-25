1d90ddfa1068d6b651e6b383cc9579efaf55cd6a
sriramgandrothu <sriramgandrothu@gmail.com>
2026-05-25 10:42:12 +0530

    server.js

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const port = process.env.PORT || 4004;
 
 app.use("/project1", express.static(path.join(__dirname, "app", "project1", "webapp")));
 app.get("/favicon.ico", (_req, res) => res.status(204).end());
+app.get("/sap/bc/lrep/flex/data/:appId", (_req, res) => res.json({ changes: [] }));
+app.get("/sap/bc/lrep/flex/settings", (_req, res) =>
+  res.json({
+    isKeyUser: false,
+    isAtoAvailable: false,
+    isProductiveSystem: true
+  })
+);
 app.get("/", (_req, res) => res.redirect("/project1/index.html"));
 
 cds.serve("all").in(app).then(() => {
